e386209e725845b31e729e85331b316dbce3d0d9
Ellen Marie Dash <me@duckie.co>
2021-08-12 20:25:51 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+
+apt-get update -y
+
+apt-get install -y liquidsoap icecast2 caddy
+
+ufw enable
+ufw allow 22,80/tcp
+
+systemctl enable icecast2
